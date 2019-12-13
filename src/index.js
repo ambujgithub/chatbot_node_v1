276e1988fe63a615651620e3ea7453cf2e25549e
Ambuj Bhatnagar <erambuj92@gmail.com>
2019-12-13 23:36:44 +0530

    added new message functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
 const express = require('express');
+const socketIO = require('socket.io');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
-const app = express();
+const app = express(); 
 const publicPath = path.join(__dirname,'../public');
 
 app.use(express.static(publicPath));
 
-app.listen(PORT,()=>console.log(`Server is running on port: ${PORT}`))
+const server = app.listen(PORT,()=>{
+    console.log(`Server is running on port: ${PORT}`);
+})
+
+const io = socketIO(server);
+
+io.on('connection',(socket)=>{
+    console.log(`New User connected`);
+
+    socket.emit('newMessage',{
+        from: 'Admin',
+        text: 'Welcome to the Chat App',
+        createdAt: new Date().getTime()
+    })
+
+    socket.broadcast.emit('newMessage',{
+        from: 'Admin',
+        text: 'New User Joined',
+        createdAt: new Date().getTime()
+    })
+
+
+    socket.on('createMessage',(message)=>{
+        console.log(`Create message: ${message.text}`);
+        io.emit('newMessage',{
+            from: 'Ambuj',
+            message: 'Hello',
+            createdAt: new Date().getTime()
+        });
+    });
+
+    socket.on('disconnect',()=>{
+        console.log('disconnected from client');
+    });
+})
